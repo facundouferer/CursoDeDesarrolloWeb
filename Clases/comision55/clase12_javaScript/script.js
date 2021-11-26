e2b27be6562a6d40e1007e88a25a7d49700fcd62
Facundo Uferer <juanfacundouf@gmail.com>
2021-11-26 18:40:05 -0300

    LO QUE NECESITABA CRISTIAN

--- Clases/comision55/clase12_javaScript/script.js
+++ Clases/comision55/clase12_javaScript/script.js
@@ -1,11 +1,19 @@
-function mostrarMensaje(parametro) {
-    parametro.preventDefault();
+
+function mostrarMensaje() {
+
+    // var VARIABLE = ALGO_QUE_INGRESA_A_LA_VARIABLE
+    
     var uno = document.querySelector("#uno").value;
     var dos = document.querySelector("#dos").value;
-    var resultado = parseFloat(uno)/parseFloat(dos);
-    mensaje = uno + " / " + dos+" = "+resultado;
+    
+    var resultado = parseFloat(uno)+parseFloat(dos);
+
+    mensaje = uno + " + " + dos + " = " + resultado;
+
     document.querySelector("#mensaje").textContent = mensaje;    
+
 }
 
 var formulario = document.querySelector("#formulario");
+
 formulario.addEventListener("input", mostrarMensaje);
