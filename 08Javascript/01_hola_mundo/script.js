fe8b351917362d502934f4ac812d07ee923c2492
Facundo Uferer <juanfacundouf@gmail.com>
2022-06-30 19:49:41 -0300

    javascript

--- 08Javascript/01_hola_mundo/script.js
+++ 08Javascript/01_hola_mundo/script.js
@@ -1,1 +1,20 @@
-console.log("Hola mundo");
+var resultado = 0;
+var num1 = 5;
+var num2 = 2;
+resultado = num1 + num2;
+document.write(num1 + " + " + num2 + " = " + (num1 + num2));
+document.write("<br>");
+document.write(num1 + " - " + num2 + " = " + (num1 - num2));
+document.write("<br>");
+document.write(num1 + " x " + num2 + " = " + (num1 * num2));
+document.write("<br>");
+
+var dividendo = 17;
+var divisor = 6;
+var resto = dividendo % divisor;
+var cociente = dividendo / divisor;
+resultado = Math.trunc(cociente);
+document.write("<h3>División Entera</h3>");
+document.write(dividendo + " / " + divisor + " = " + resultado);
+document.write("<br>");
+document.write(" Resto: " + resto);
