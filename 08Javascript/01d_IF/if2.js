fed268928e6f5bb9f078e238502b08b508ae7f95
Facundo Uferer <juanfacundouf@gmail.com>
2022-07-13 09:03:07 -0300

    input

--- 08Javascript/01d_IF/if2.js
+++ 08Javascript/01d_IF/if2.js
@@ -1,8 +1,15 @@
-var edad = 37;
-var nombre = "Facundo";
+var btn = document.getElementById("btn");
 
-if (edad >= 18) {
-    console.log(nombre + " es mayor de edad");
-} else {
-    console.log(nombre + " es menor de edad");
+function calcularEdad(e) {
+    e.preventDefault();
+    var nombre = "Facundo";
+    var edad = document.getElementById("edad").value;
+    var resultado = document.querySelector("h1");
+    if (edad >= 18) {
+        resultado.innerHTML = "Hola " + nombre + " eres mayor de edad";
+    } else {
+        resultado.innerHTML = "Hola " + nombre + " eres menor de edad";
+    }
 }
+
+btn.addEventListener("click", calcularEdad);
